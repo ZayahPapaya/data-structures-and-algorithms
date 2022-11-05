4ffce4c4462eae929c76b4db79c3e550d63b4d86
Aiden <hyena.shenanigans@gmail.com>
2022-11-05 15:06:36 -0700

    Adds graph methods and testing

--- 401-javascript/cc-35/app.test.ts
+++ 401-javascript/cc-35/app.test.ts
@@ -1,4 +1,4 @@
-import { Node, Edge, Item, Location,  neighbors } from "./app";
+import { Node, Edge, Item, Location, Graph } from "./app";
 
 let dhammer: Node<Item, Location> = {
   value: { name: 'Dragon Warhammer' },
@@ -20,13 +20,31 @@ let pk: Edge<Item, Location> = {
   nodes: [gmaul, dhammer],
 }
 
-
+let graph = new Graph<Item, Location>();
 describe('A graph', () => {
-  
-  it('Graphs things', () => {
-    dhammer.edges.push(wilderness, pk);
-    gmaul.edges.push(pk)
-    expect(neighbors(gmaul)).toEqual(new Set([dhammer]));
+
+  it('Adds nodes', () => {
+    let a = graph.addNode(dhammer);
+    let b = graph.addNode(gmaul);
+    expect(a).toBe(dhammer);
+    expect(b).toBe(gmaul);
   })
+
+  it('Builds edges', () => {
+    graph.addEdge(dhammer, gmaul, {name: 'Wilderness'})
+    expect(dhammer.edges.length).toBe(1)
+    expect(dhammer.edges[0].value.name).toBe('Wilderness')
+  })
+
+  it('Graphs neighbors', () => {
+    expect(graph.getNeighbors(gmaul).size).toEqual(1);
+  });
+
+  it('Graphs everything', () => {
+    expect(graph.getNodes().has(dhammer)).toEqual(true)
+  });
+  it('Has a size', () => {
+    expect(graph.size()).toBe(2);
+  });
 
 })
