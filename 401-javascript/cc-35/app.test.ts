c2dd41c2c7b5d6f64a3279f95c675d66ffbeb7e0
Aiden <hyena.shenanigans@gmail.com>
2022-11-07 23:18:08 -0800

    completes code challenge 36

--- 401-javascript/cc-35/app.test.ts
+++ 401-javascript/cc-35/app.test.ts
@@ -10,16 +10,6 @@ let gmaul: Node<Item, Location> = {
   edges: [],
 }
 
-let wilderness: Edge<Item, Location> = {
-  value: { name: 'Wilderness' },
-  nodes: [dhammer, gmaul],
-}
-
-let pk: Edge<Item, Location> = {
-  value: { name: 'PvP' },
-  nodes: [gmaul, dhammer],
-}
-
 let graph = new Graph<Item, Location>();
 describe('A graph', () => {
 
@@ -43,8 +33,62 @@ describe('A graph', () => {
   it('Graphs everything', () => {
     expect(graph.getNodes().has(dhammer)).toEqual(true)
   });
+  
   it('Has a size', () => {
     expect(graph.size()).toBe(2);
   });
 
-})
+});
+
+let pandora: Node<Item, Location> = {
+  value: { name: 'Pandora' },
+  edges: [],
+}
+
+let arendelle: Node<Item, Location> = {
+  value: { name: 'Arendelle' },
+  edges: [],
+}
+
+let metroville: Node<Item, Location> = {
+  value: { name: 'Metroville' },
+  edges: [],
+}
+
+let monstropolis: Node<Item, Location> = {
+  value: { name: 'Monstropolis' },
+  edges: [],
+}
+
+let narnia: Node<Item, Location> = {
+  value: { name: 'Narnia' },
+  edges: [],
+}
+
+let naboo: Node<Item, Location> = {
+  value: { name: 'Naboo' },
+  edges: [],
+}
+
+let graph2 = new Graph<Item, Location>();
+
+describe('A graph', () => {
+  graph2.addNode(pandora)
+  graph2.addNode(arendelle)
+  graph2.addNode(metroville)
+  graph2.addNode(monstropolis)
+  graph2.addNode(narnia)
+  graph2.addNode(naboo)
+  graph2.addEdge(pandora, arendelle, {name: 'edge'})
+  graph2.addEdge(arendelle, metroville, {name: 'edge'})
+  graph2.addEdge(arendelle, monstropolis, {name: 'edge'})
+  graph2.addEdge(metroville, monstropolis, {name: 'edge'})
+  graph2.addEdge(metroville, narnia, {name: 'edge'})
+  graph2.addEdge(metroville, naboo, {name: 'edge'})
+  graph2.addEdge(monstropolis, naboo, {name: 'edge'})
+  graph2.addEdge(narnia, naboo, {name: 'edge'})
+
+  it('Breads', () => {
+    expect(graph2.bread(graph2.getNodes())).toEqual(["Pandora", "Arendelle", "Metroville", "Monstropolis", "Narnia", "Naboo"])
+  })
+});
