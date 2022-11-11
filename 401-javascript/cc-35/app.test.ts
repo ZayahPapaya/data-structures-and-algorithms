2fa2643aaa0d9136ee8cd32dfc2f4724ccee5980
Aiden <hyena.shenanigans@gmail.com>
2022-11-11 01:39:26 -0800

    Adds businessTrip

--- 401-javascript/cc-35/app.test.ts
+++ 401-javascript/cc-35/app.test.ts
@@ -1,4 +1,4 @@
-import { Node, Edge, Item, Location, Graph } from "./app";
+import { Node, Edge, Item, Location, Graph, businessTrip } from "./app";
 
 let dhammer: Node<Item, Location> = {
   value: { name: 'Dragon Warhammer' },
@@ -10,28 +10,29 @@ let gmaul: Node<Item, Location> = {
   edges: [],
 }
 
-let graph = new Graph<Item, Location>();
+let graph = new Graph<string, string>();
 describe('A graph', () => {
-
+  let a;
+  let b;
   it('Adds nodes', () => {
-    let a = graph.addNode(dhammer);
-    let b = graph.addNode(gmaul);
-    expect(a).toBe(dhammer);
-    expect(b).toBe(gmaul);
+    a = graph.addNode('Dragon Warhammer');
+    b = graph.addNode('Granite Maul');
+    expect(a.value).toBe('Dragon Warhammer');
+    expect(b.value).toBe('Granite Maul');
   })
 
   it('Builds edges', () => {
-    graph.addEdge(dhammer, gmaul, {name: 'Wilderness'})
-    expect(dhammer.edges.length).toBe(1)
-    expect(dhammer.edges[0].value.name).toBe('Wilderness')
+    graph.addEdge(a, b, 'Wilderness')
+    expect(a.edges.length).toBe(1)
+    expect(a.edges[0].value).toBe('Wilderness')
   })
 
   it('Graphs neighbors', () => {
-    expect(graph.getNeighbors(gmaul).size).toEqual(1);
+    expect(graph.getNeighbors(a).size).toEqual(1);
   });
 
   it('Graphs everything', () => {
-    expect(graph.getNodes().has(dhammer)).toEqual(true)
+    expect(graph.getNodes().has(a)).toEqual(true)
   });
   
   it('Has a size', () => {
@@ -40,55 +41,36 @@ describe('A graph', () => {
 
 });
 
-let pandora: Node<Item, Location> = {
-  value: { name: 'Pandora' },
-  edges: [],
-}
-
-let arendelle: Node<Item, Location> = {
-  value: { name: 'Arendelle' },
-  edges: [],
-}
-
-let metroville: Node<Item, Location> = {
-  value: { name: 'Metroville' },
-  edges: [],
-}
-
-let monstropolis: Node<Item, Location> = {
-  value: { name: 'Monstropolis' },
-  edges: [],
-}
-
-let narnia: Node<Item, Location> = {
-  value: { name: 'Narnia' },
-  edges: [],
-}
-
-let naboo: Node<Item, Location> = {
-  value: { name: 'Naboo' },
-  edges: [],
-}
-
-let graph2 = new Graph<Item, Location>();
+let graph2 = new Graph<string, number>();
 
 describe('A graph', () => {
-  graph2.addNode(pandora)
-  graph2.addNode(arendelle)
-  graph2.addNode(metroville)
-  graph2.addNode(monstropolis)
-  graph2.addNode(narnia)
-  graph2.addNode(naboo)
-  graph2.addEdge(pandora, arendelle, {name: 'edge'})
-  graph2.addEdge(arendelle, metroville, {name: 'edge'})
-  graph2.addEdge(arendelle, monstropolis, {name: 'edge'})
-  graph2.addEdge(metroville, monstropolis, {name: 'edge'})
-  graph2.addEdge(metroville, narnia, {name: 'edge'})
-  graph2.addEdge(metroville, naboo, {name: 'edge'})
-  graph2.addEdge(monstropolis, naboo, {name: 'edge'})
-  graph2.addEdge(narnia, naboo, {name: 'edge'})
+  const pandora = graph2.addNode('Pandora')
+  const arendelle = graph2.addNode('Arendelle')
+  const metroville = graph2.addNode('Metroville')
+  const monstropolis = graph2.addNode('Monstropolis')
+  const narnia = graph2.addNode('Narnia')
+  const naboo = graph2.addNode('Naboo')
+  graph2.addEdge(pandora, arendelle, 150)
+  graph2.addEdge(arendelle, metroville, 82)
+  graph2.addEdge(arendelle, monstropolis, 42)
+  graph2.addEdge(metroville, monstropolis, 105)
+  graph2.addEdge(metroville, narnia, 37)
+  graph2.addEdge(metroville, naboo, 26)
+  graph2.addEdge(monstropolis, naboo, 73)
+  graph2.addEdge(narnia, naboo, 250)
 
   it('Breads', () => {
-    expect(graph2.bread(graph2.getNodes())).toEqual(["Pandora", "Arendelle", "Metroville", "Monstropolis", "Narnia", "Naboo"])
+    expect(graph2.bread(graph2.nodes)).toEqual(["Pandora", "Arendelle", "Metroville", "Monstropolis", "Narnia", "Naboo"])
+  })
+  
+  it('Does trips', () => {
+    expect(businessTrip(graph2, [metroville, pandora])).toBe(82)
+    expect(businessTrip(graph2, [arendelle, monstropolis, naboo])).toBe(115)
+  })
+
+  it(`Doesn't do trips`, () => {
+    expect(businessTrip(graph2, [naboo, pandora])).toBe(null)
+    expect(businessTrip(graph2, [narnia, arendelle, naboo])).toBe(null)
   })
 });
+// businessTrip is O(n * e) time and O(1) space
--- 401-javascript/cc-35/app.ts
+++ 401-javascript/cc-35/app.ts
@@ -1,3 +1,4 @@
+import { continueStatement } from "@babel/types";
 
 
 export interface Node<NV, EV> {
@@ -23,9 +24,13 @@ export class Graph<NV, EV> {
   constructor() {
     this.nodes = new Set();
   }
-  addNode = (value: Node<NV, EV>): Node<NV, EV> => {
-    this.nodes.add(value);
-    return value;
+  addNode = (value: NV): Node<NV, EV> => {
+    let node: Node<NV, EV> = {
+      value,
+      edges: []
+    }
+    this.nodes.add(node);
+    return node;
   }
   getNodes = (): Set<Node<NV, EV>> => {
     return this.nodes
@@ -50,7 +55,7 @@ export class Graph<NV, EV> {
 
   bread = (loaf: Set<Node<NV, EV>>): any => {
     let visited: any = new Set();
-    let result = [];
+    let result: any = [];
     let queue: any = [];
     if (loaf.size > 0) {
       const iter = loaf.values();
@@ -62,14 +67,11 @@ export class Graph<NV, EV> {
 
     while (queue.length > 0) {
       let current: Node<NV, EV> = queue.shift();
-      console.log('current node', current)
       if (visited.has(current.value)) {
-        console.log('continue')
         continue;
       } else {
         visited.add(current.value);
       }
-      console.log('edges', current)
       if (current.edges.length > 0) {
         current.edges.forEach(edge => {
           edge.nodes.forEach(node => {
@@ -78,13 +80,50 @@ export class Graph<NV, EV> {
         })
       }
     }
-    visited.forEach((item: any) => {
-      result.push(item.name);
-    })
+    visited.forEach((item: any) => result.push(item))
     return result;
   }
+
+  // businessTrip = (route: Node<string, number>[]): number | null => {
+  //   let tracker: any = Array.from(this.nodes).find((city: any) => city === route[0]);
+  //   if (!tracker) return null;
+  //   let cost: number = 0;
+  //   let visited: any = new Set();
+  //   let queue: any = [];
+  //   while (queue.length > 0) {
+  //     let current: Node<string, number> = queue.shift();
+  //     if (visited.has(current.value)) continue;
+  //     visited.add(current.value);
+  //     if (current.edges.length > 0) {
+  //       current.edges.forEach(edge => {
+  //         edge.nodes.forEach(node => {
+  //           if (route.find(element => element.value === node.value)) {
+  //             queue.push(node);
+  //             cost = edge.value + cost;
+  //           }
+  //         })
+  //       })
+  //     }
+  //   }
+  //   return cost;
+  // }
+
 }
 
+export const businessTrip = (graph: Graph<string, number>, route: Node<string, number>[]): number | null => {
+  let cost = 0;
+  let tracker = route.shift()!;
+  while(route.length > 0) {
+    let neighbors = graph.getNeighbors(tracker);
+    let next = route.shift()!;
+    console.log(next)
+    console.log(neighbors)
+    if(!neighbors.has(next)) return null;
+    cost += tracker.edges.find((edge: Edge<string, number>) =>  edge.nodes[1] === next)?.value ?? 0;
+    tracker = next;
+  }
+  return cost;
+}
 
 
 
