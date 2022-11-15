bd5aa78407339c8cce1af82feab12385deb701f4
Aiden <hyena.shenanigans@gmail.com>
2022-11-14 20:39:01 -0800

    Adds mazes code challenge

--- 401-javascript/cc-35/app.test.ts
+++ 401-javascript/cc-35/app.test.ts
@@ -1,4 +1,4 @@
-import { Node, Edge, Item, Location, Graph, businessTrip } from "./app";
+import { Node, Edge, Item, Location, Graph, businessTrip, solveMaze, Room, Door } from "./app";
 
 let dhammer: Node<Item, Location> = {
   value: { name: 'Dragon Warhammer' },
@@ -34,7 +34,7 @@ describe('A graph', () => {
   it('Graphs everything', () => {
     expect(graph.getNodes().has(a)).toEqual(true)
   });
-  
+
   it('Has a size', () => {
     expect(graph.size()).toBe(2);
   });
@@ -62,7 +62,7 @@ describe('A graph', () => {
   it('Breads', () => {
     expect(graph2.bread(graph2.nodes)).toEqual(["Pandora", "Arendelle", "Metroville", "Monstropolis", "Narnia", "Naboo"])
   })
-  
+
   it('Does trips', () => {
     expect(businessTrip(graph2, [metroville, pandora])).toBe(82)
     expect(businessTrip(graph2, [arendelle, monstropolis, naboo])).toBe(115)
@@ -74,3 +74,45 @@ describe('A graph', () => {
   })
 });
 // businessTrip is O(n * e) time and O(1) space
+// export interface Door {
+//   id: number;
+//   status: 'locked' | 'unlocked';
+//   color: 'red' | 'green' | 'blue' | undefined;
+// }
+
+// export interface Room {
+//   id: number;
+//   contents: 'key' | 'treasure' | undefined;
+//   color: 'red' | 'green' | 'blue' | undefined;
+// }
+let maze = new Graph<Room, Door>();
+describe('A solvable maze!', () => {
+  const start = maze.addNode({ id: 1, contents: undefined, color: undefined });
+  const middle = maze.addNode({ id: 2, contents: undefined, color: undefined });
+  const keyRoom = maze.addNode({ id: 3, contents: 'key', color: 'blue' });
+  const treasureRoom = maze.addNode({ id: 4, contents: 'treasure', color: undefined });
+
+  maze.addEdge(start, middle, { id: 5, status: 'unlocked', color: undefined });
+  maze.addEdge(middle, treasureRoom, { id: 6, status: 'locked', color: 'blue' });
+  maze.addEdge(start, keyRoom, { id: 7, status: 'unlocked', color: undefined });
+
+  it('Wins the maze', () => {
+    expect(solveMaze(start)).toBe(true);
+  });
+});
+
+let maze2 = new Graph<Room, Door>();
+describe('A not solvable maze :c', () => {
+  const start = maze2.addNode({ id: 1, contents: undefined, color: undefined });
+  const middle = maze2.addNode({ id: 2, contents: 'key', color: 'blue' });
+  const keyRoom = maze2.addNode({ id: 3, contents: undefined, color: undefined });
+  const treasureRoom = maze2.addNode({ id: 4, contents: 'treasure', color: undefined });
+
+  maze2.addEdge(start, middle, { id: 5, status: 'locked', color: 'blue' });
+  maze2.addEdge(middle, treasureRoom, { id: 6, status: 'unlocked', color: undefined });
+  maze2.addEdge(start, keyRoom, { id: 7, status: 'unlocked', color: undefined });
+
+  it('Loses the maze', () => {
+    expect(solveMaze(start)).toBe(false);
+  })
+})
